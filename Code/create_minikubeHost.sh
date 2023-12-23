7b3e2bddf2acd76c85c0c38a5723112ec04bb90e
Rafael Urrutia Silva <84277015+rafaelurrutiasilva@users.noreply.github.com>
2023-12-23 17:34:49 +0100

    Update create_minikubeHost.sh

--- Code/create_minikubeHost.sh
+++ Code/create_minikubeHost.sh
@@ -1,1 +1,41 @@
+#!/bin/bash
+#
 
+if [[ ! $(uname -a) =~ "photon" ]];then
+    echo "You are not running Photon OS as Linux OS!"
+    echo "You need to replace 'tdnf' package manager whit the one used in your Linux OS."
+    exit
+fi
+
+# Start ENVs
+myHOSTNAME=minikubeHost
+
+# Install beasic tool
+echo "Installing beasic tool and setting the hostname"
+hostnamectl hostname $myHOSTNAME
+tdnf install sudo tar  -y
+tdnf update -y
+
+# Post installation and configuration for Docker
+echo "Post installation and configuration for Docker"
+systemctl enable docker
+systemctl start docker
+useradd  -m labuser
+usermod -aG docker labuser
+
+# Minikube install
+echo "Installing latest minikube stable releas"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install -m 755 minikube-linux-amd64 /usr/local/bin/minikube
+rm minikube-linux-amd64
+
+# Installing latest kubectl stable releas.
+echo "Installing latest kubectl stable releas"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+
+# Reboot needed to sett the hostname
+echo "The host will now bee rebooted..."
+sleep 5
+reboot
